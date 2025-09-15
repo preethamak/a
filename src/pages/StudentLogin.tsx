71b53effede16a1551430628d3a579789c3cba7b
forunofficialuse03-cyber <forunofficialuse03@gmail.com>
2025-09-15 15:20:54 +0530

    Update StudentLogin.tsx

--- src/pages/StudentLogin.tsx
+++ src/pages/StudentLogin.tsx
@@ -12,7 +12,7 @@ const StudentLogin = () => {
   const [name, setName] = useState('');
   const [rollNumber, setRollNumber] = useState('');
   const [adminRoll, setAdminRoll] = useState('');
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(true);
   const navigate = useNavigate();
 
   const handleStudentLogin = () => {
